b46d052d7911c7c943b9a6db108e8ab176d57920
hunter <hunter@hunter>
2025-07-21 07:20:40 +0530

    feat: Integrate Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Converter from './Converter';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <Converter />
+      <Analytics />
     </div>
   );
 }
